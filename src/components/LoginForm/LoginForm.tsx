e4cc1f26e418a9635896ccfd0270097604c8cf0b
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-09-21 15:18:39 +0200

    feat: create login UI

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,46 @@
+﻿import React from 'react';
+import { TextField, Button } from 'nerdux-ui-system';
+
+import styles from './LoginForm.module.scss';
+
+export const LoginForm = () => {
+   return (
+      <form className={styles.loginForm} action="">
+         <div className={styles.loginForm__inputBox}>
+            <TextField
+               error=""
+               hint=""
+               id="email"
+               label="Email"
+               name="email"
+               onBlur={function noRefCheck() {}}
+               onChange={function noRefCheck() {}}
+               onClear={function noRefCheck() {}}
+               onFocus={function noRefCheck() {}}
+               placeholder="Your email"
+               type="text"
+               value=""
+            />
+            <TextField
+               error=""
+               hint=""
+               id="password"
+               label="Password"
+               name="password"
+               onBlur={function noRefCheck() {}}
+               onChange={function noRefCheck() {}}
+               onClear={function noRefCheck() {}}
+               onFocus={function noRefCheck() {}}
+               placeholder="Your password"
+               type="password"
+               value=""
+            />
+         </div>
+         <div className={styles.loginForm__buttonBox}>
+            <Button onClick={function noRefCheck() {}} type="submit" variant="primary">
+               Submit
+            </Button>
+         </div>
+      </form>
+   );
+};
