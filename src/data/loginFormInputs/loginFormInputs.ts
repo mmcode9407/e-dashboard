a7afae1b22b3ec7efb5adc1495d056ea42902efd
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-09-22 12:22:50 +0200

    feat: add form handling and validating

---
+++ src/data/loginFormInputs/loginFormInputs.ts
@@ -0,0 +1,38 @@
+﻿export interface ILoginInputs {
+   id: string;
+   label: string;
+   name: InputName;
+   placeholder: string;
+   type: 'text' | 'password';
+   required: boolean;
+   pattern?: string;
+   errMessage?: string;
+}
+
+enum InputName {
+   EMAIL = 'email',
+   PASSWORD = 'password',
+}
+
+const loginInputs: ILoginInputs[] = [
+   {
+      id: 'email',
+      label: 'Email',
+      name: InputName.EMAIL,
+      placeholder: 'Your email',
+      type: 'text',
+      required: true,
+      pattern: '[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}',
+      errMessage: 'Invalid email address',
+   },
+   {
+      id: 'password',
+      label: 'Password',
+      name: InputName.PASSWORD,
+      placeholder: 'Your password',
+      type: 'password',
+      required: true,
+   },
+];
+
+export default loginInputs;
---
+++ src/utils/validateForm/validateForm.ts
@@ -0,0 +1,26 @@
+﻿import { IFormValues } from 'components/LoginForm/LoginForm';
+import { ILoginInputs } from 'data/loginFormInputs/loginFormInputs';
+
+const validateForm = (values: IFormValues, fields: ILoginInputs[]) => {
+   const errors: Partial<IFormValues> = {};
+
+   fields.forEach((field) => {
+      const { name, required, pattern, errMessage } = field;
+      const value = values[name];
+
+      if (required && !value) {
+         errors[name] = 'Required';
+      }
+
+      if (pattern) {
+         const reg = new RegExp(pattern, 'i');
+         if (!reg.test(value)) {
+            errors[name] = errMessage;
+         }
+      }
+   });
+
+   return errors;
+};
+
+export default validateForm;
