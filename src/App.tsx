36313cca0fb01bc0e529a612e4381d6b21800b13
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-10-02 13:35:54 +0200

    feat: save paths as enum for greater reusability

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,35 @@ import { Dashboard } from 'views/Dashboard';
 import { Leads } from 'views/Leads';
 import { RequireAuth, useIsAuthenticated } from 'react-auth-kit';
 
+enum Paths {
+   HOME = '/',
+   LOGIN = '/login',
+   DASHBOARD = '/dashboard',
+   LEADS = '/leads',
+}
+
 function App() {
    const isAuthenticated = useIsAuthenticated();
 
    return (
       <Routes>
-         <Route path="/" element={<Navigate to="/login" />} />
+         <Route path={Paths.HOME} element={<Navigate to={Paths.LOGIN} />} />
          <Route
-            path="/login"
-            element={!isAuthenticated() ? <Login /> : <Navigate to="/dashboard" />}
+            path={Paths.LOGIN}
+            element={!isAuthenticated() ? <Login /> : <Navigate to={Paths.DASHBOARD} />}
          />
          <Route
-            path="/dashboard"
+            path={Paths.DASHBOARD}
             element={
-               <RequireAuth loginPath="/login">
+               <RequireAuth loginPath={Paths.LOGIN}>
                   <Dashboard />
                </RequireAuth>
             }
          />
          <Route
-            path="/leads"
+            path={Paths.LEADS}
             element={
-               <RequireAuth loginPath="/login">
+               <RequireAuth loginPath={Paths.LOGIN}>
                   <Leads />
                </RequireAuth>
             }
